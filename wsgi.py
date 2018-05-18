a427317a3b4dc1ac1082485d6f0951d274a28f7c
pathakala <pachakala@yahoo.com>
2018-05-18 13:11:28 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,14 +2,9 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, '''''!</h1>
-    </body>
-</html>'''
-
+ def hello():	 =
+ return "Hello, Janus Henderson!"
+ 	 
+ 
 if __name__ == "__main__":
     application.run()
