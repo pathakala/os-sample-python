138f4c3e6e60ccb518e2c5522d07c8ed81b62f9d
Patty Hakala <patricia.hakala@janus.com>
2018-05-18 13:56:54 -0600

    attempt to add templates

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     application.run()
