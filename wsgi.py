1eecebbf73e18bc7e686df869c66849c7b657201
pathakala <pachakala@yahoo.com>
2018-05-18 12:50:45 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,8 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello, Janus!"
+    return "Hello, Janus Henderson!"
+    return
 
 if __name__ == "__main__":
     application.run()
