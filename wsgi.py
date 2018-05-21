b4095f7d83842d6ea3307257618b15fc3a8ef3e6
Patty Hakala <patricia.hakala@janus.com>
2018-05-21 08:58:16 -0600

    no message

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,9 @@
-from flask import Flask, request, send_from_directory
-
-# set the project root directory as the static folder, you can set others.
+from flask import Flask, request
 app = Flask(__name__, static_url_path='')
 
-
-	@app.route('/<path:path>')
-def static_file(path):
-    return app.send_static_file(path)
+@app.route('/index/')
+def root():
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
