f080a7977abfcb3534528f0386c3830b12698c6f
Patty Hakala <patricia.hakala@janus.com>
2018-05-21 08:32:20 -0600

    test

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, Markup
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return render_template('index.html')
+def test_escaping(self):
+        text = '<p>Hello World!'
+        app = flask.Flask(__name__)
+        @app.route('/')
+        def index():
+            return flask.render_template('escaping_template.html', text=text,
+                                         html=flask.Markup(text))
+        lines = app.test_client().get('/').data.splitlines()
+        self.assert_equal(lines, [
+            b'&lt;p&gt;Hello World!',
+            b'<p>Hello World!',
+            b'<p>Hello World!',
+            b'<p>Hello World!',
+            b'&lt;p&gt;Hello World!',
+            b'<p>Hello World!'
+        ]) 
 
 if __name__ == "__main__":
     application.run()
